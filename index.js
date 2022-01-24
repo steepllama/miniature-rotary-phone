b875f396e5d03a85559169e3e21e1b8c843b8472
Miguel Vazquez <migueldv90@yahoo.com>
2022-01-24 00:01:18 -0600

    update

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
-const Employee = require("./lib/Employee");
-// const Intern = require('./lib/Intern');
-// const Manager = require('./lib/Manager');
-// const Engineer = require('./lib/Engineer');
-// const { exit } = require('process');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
+const { exit } = require('process');
 
 let teamMembers = [];
 
@@ -17,37 +14,37 @@ const bossQuestions = () => {
       {
         type: "input",
         name: "name",
-        message: `What is the Manager's name?`,
+        message: 'What is the Manager\'s name?',
       },
       {
         type: "input",
         name: "id",
-        message: `What is the Manager's id number?`,
+        message: 'What is the Manager\'s id number?',
       },
       {
         type: "input",
         name: "email",
-        message: `What is the Manager's email?`,
+        message: 'What is the Manager\'s email?',
       },
       {
         type: "input",
         name: "officeNumber",
-        message: `What is the Manager's office number? (Required)`,
+        message: 'What is the Manager\'s office number? (Required)',
       },
       {
         type: "list",
         name: "addPeople",
         message: "Would you like to add another employee?",
-        choices: [`Engineer`, `Intern`, `Neither`],
+        choices: ['Engineer', 'Intern', 'Neither'],
       },
     ])
-    .then(( answer ) => {
-      const manager = new Manager(
-        answer.name,
-        answer.id,
-        answer.email,
-        answer.officeNumber
-      );
+    .then((answer) => {
+      const manager = {
+        name: answer.name,
+        id: answer.id,
+        email: answer.email,
+        officeNumber: answer.officeNumber
+      };
       teamMembers.push(manager);
       console.log(teamMembers);
       switch (answer.addPeople) {
@@ -73,37 +70,39 @@ const EngineerQuestions = () => {
       {
         type: "input",
         name: "name",
-        message: `What is the Engineer's name?`,
+        message: 'What is the Engineer\'s name?',
       },
       {
         type: "input",
         name: "id",
-        message: `What is the Engineer's id?`,
+        message: 'What is the Engineer\'s id?',
       },
       {
         type: "input",
         name: "email",
-        message: `What is the Engineer's email?`,
+        message: 'What is the Engineer\'s email?',
       },
       {
         type: "input",
         name: "github",
-        message: `What is the Engineer's Github profile?`,
+        message: 'What is the Engineer\'s Github profile?',
       },
       {
         type: "list",
         name: "addPeople",
-        message: [`Engineer, Intern, Neither`],
+        message: "Would you like to add another employee?",
+        choices: ['Engineer', 'Intern', 'Neither'],
       },
     ])
-    .then(( answer ) => {
-      const engineer = new Engineer(
-        answer.name,
-        answer.id,
-        answer.email,
-        answer.github
-      );
+    .then((answer) => {
+      const engineer = {
+        name: answer.name,
+        id: answer.id,
+        email: answer.email,
+        github: answer.github
+      };
       teamMembers.push(engineer);
+      console.log(teamMembers);
       switch (answer.addPeople) {
         case "Engineer":
           EngineerQuestions();
@@ -127,37 +126,39 @@ const InternQuestions = () => {
       {
         type: "input",
         name: "name",
-        message: `What is the Intern's name?`,
+        message: 'What is the Intern\'s name?',
       },
       {
         type: "input",
         name: "id",
-        message: `What is the Intern's Id?`,
+        message: 'What is the Intern\'s Id?',
       },
       {
         type: "input",
         name: "email",
-        message: `What is the Intern's email?`,
+        message: 'What is the Intern\'s email?',
       },
       {
         type: "input",
         name: "school",
-        message: `What school does the Intern attend?`,
+        message: 'What school does the Intern attend?',
       },
       {
         type: "list",
         name: "addPeople",
-        message: [`Engineer, Intern, Neither`],
+        message: "Would you like to add another employee?",
+        choices: ['Engineer', 'Intern', 'Neither'],
       },
     ])
-    .then(( answer ) => {
-      const intern = new Intern(
-        answer.name,
-        answer.id,
-        answer.email,
-        answer.school
-      );
+    .then((answer) => {
+      const intern = {
+        name: answer.name,
+        id: answer.id,
+        email: answer.email,
+        school: answer.school
+      };
       teamMembers.push(intern);
+      console.log(teamMembers);
       switch (answer.addPeople) {
         case "Engineer":
           EngineerQuestions();
@@ -179,7 +180,7 @@ bossQuestions();
 
 function writeToFile(filename, data) {
   fs.writeFile(filename, data, (err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log('Success!')
   })
 }
